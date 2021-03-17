c322bad3d8b18363281522b543cefda2768a908a
Davys Lima <davysjunior08@hotmail.com>
2021-03-17 19:46:57 -0300

    Feature: Challenger - Terminated.

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+interface GenreResponseProps {
+  selectedGenre: {
+    title: string;
+  };
+}
+
+export function Header({ selectedGenre }: GenreResponseProps) {
+  return (
+    <header>
+      <span className="category">
+        Categoria:<span> {selectedGenre.title}</span>
+      </span>
+    </header>
+  );
+}
